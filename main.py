38ab63a6c6147a9849f00485344f3af087cc50eb
mgrischy1 <mikegrischy@gmail.com>
2022-08-29 15:46:07 -0700

    Add docstring to methods

--- main.py
+++ main.py
@@ -1,7 +1,6 @@
 import datetime
 import re
 from dataclasses import dataclass
-from datetime import time
 from PyQt5 import QtCore, QtGui, QtWidgets
 import mysql.connector
 from PyQt5.QtWidgets import QMessageBox
@@ -9,43 +8,16 @@ from PyQt5.QtWidgets import QMessageBox
 
 @dataclass
 class AssemblyRegex:
-    """Holds product name of DUT"""
+    """AssemblyRegex object that sets part number regex, serial number regex and station_name"""
     pn_rx: str
     sn_rx: str
     station_name: str
 
 
-@dataclass
-class Operator:
-    """Holds product name of DUT"""
-    user_name: str
-
-
-@dataclass
-class StationReport:
-    """Holds product name of DUT"""
-    station_name: str
-    operator_user_name: str
-    dut_part_number: str
-    dut_serial_number: str
-    status: str
-    started_at: time
-    ended_at: time
-
-
-@dataclass
-class AssemblyReport:
-    """Holds product name of DUT"""
-    station_name: str
-    part_name: str
-    part_number: str
-    serial_number: str
-    installed: bool
-
-
 class Ui_Dialog(object):
 
     def __init__(self):
+        self.operator_user_name: str = ""
         self.permission: str = ""
         self.rout_name: str = ""
         self.first_pass: bool = True
@@ -59,19 +31,21 @@ class Ui_Dialog(object):
         self.dut_pn = None
         self.enable_assembly = False
         self.assembly_list: list = []
-        self.method_list = []
-        self.counter = 0
+        self.test_method_list = []
+        self.method_list_counter = 0
         self.product_name: str = ""
         self.row = 0
         self.assembly_count = 0
-        self.method_list = [self.verify_user, self.verify_pn_sn, self.verify_assembly_part]
+        self.test_method_list = [self.verify_user, self.verify_pn_sn, self.verify_assembly_part]
 
     def start(self):
-        self.cnx = mysql.connector.connect(user='root', password='123@Weezer', host='127.0.0.1',
+        """Connects and creats a local mysql instance"""
+        self.cnx = mysql.connector.connect(user='root', password='', host='127.0.0.1',
                                            database='factory_mes')
         self.cursor = self.cnx.cursor()
 
     def end(self):
+        """closes the mysql instance"""
         self.cursor.close()
         self.cnx.close()
 
@@ -140,9 +114,7 @@ class Ui_Dialog(object):
 
         self.retranslateUi(Dialog)
         QtCore.QMetaObject.connectSlotsByName(Dialog)
-        self.display_wo()
-
-
+        self.display_wo_requirements()
 
     def retranslateUi(self, Dialog):
         _translate = QtCore.QCoreApplication.translate
@@ -164,6 +136,8 @@ class Ui_Dialog(object):
         self.su_checkBox.setText(_translate("Dialog", "Super User"))
 
     def populate_combobox(self):
+        """Queries the assembly_list table for the all the station names that make up the product's rout
+        """
         self.start()
         query = (f"SELECT station_name FROM assembly_list WHERE product = '{self.product_name}';")
         self.cursor.execute(query)
@@ -174,6 +148,11 @@ class Ui_Dialog(object):
         self.test_comboBox.addItems(ret)
 
     def change_test(self):
+        """Enables the option for a superuser to change the current station for a product
+
+        Gets the selected station name from the combobox, uses the station name to query database for the
+        route position, calls the laod_assembly method
+        """
         self.station_name = self.test_comboBox.currentText()
         self.start()
         query = (f"SELECT rout_position FROM assembly_list WHERE station_name = '{self.station_name}';")
@@ -183,7 +162,13 @@ class Ui_Dialog(object):
         self.end()
         self.load_assembly()
 
-    def display_wo(self):
+    def display_wo_requirements(self):
+        """Prompts user for the required information to start a station report
+
+        Requirements:
+                Username: Operators assigned username that will be verified in the operator table
+                Work Order part number(pn) and serial number(sn): pn and sn will be verified in by the WorkOrder table
+        """
         self.tableWidget.setRowCount(2)
         self.tableWidget.setItem(0, 0, QtWidgets.QTableWidgetItem("Verify User Name"))
         self.tableWidget.setItem(0, 1, QtWidgets.QTableWidgetItem("No"))
@@ -192,25 +177,42 @@ class Ui_Dialog(object):
         self.tableWidget.setItem(1, 1, QtWidgets.QTableWidgetItem("No"))
 
     def control(self):
-        if self.method_list[self.counter] == self.verify_assembly_part and not self.enable_assembly:
+        """Tracks and executes the next test method in the test_method_list
+
+        If test_method_list element is verify_assembly_part then assembly method will append the required
+        amount of parts to be verified to the test_method_list. Once the test_method_list ends then a assembly complete
+        msg box will pop up and the gui will be reset to the display_wo_requirements state.
+        """
+        if self.test_method_list[self.method_list_counter] == self.verify_assembly_part and not self.enable_assembly:
             self.assembly()
 
-        self.method_list[self.counter]()
-        self.counter += 1
-        if self.counter == len(self.method_list):
+        self.test_method_list[self.method_list_counter]()
+        self.method_list_counter += 1
+        if self.method_list_counter == len(self.test_method_list):
             self.complete_test_msg_box()
             self.assembly_count = 0
-            self.counter = 0
+            self.method_list_counter = 0
             self.enable_assembly = False
 
     def assembly(self):
+        """Appends verify_assembly_part to test_method_list to the amount of rows in assembly
+
+        NOTE: row is populated when load_assembly is first called in the verify_pn_sn and represents the number of
+        parts that are required to be scanned in for the current assembly station to be complete
+        """
         count = 0
         while count < (self.row - 1):
-            self.method_list.append(self.verify_assembly_part)
+            self.test_method_list.append(self.verify_assembly_part)
             count += 1
         self.enable_assembly = True
 
     def complete_test_msg_box(self):
+        """Helper method called when an assembly has been completed and report is uploaded
+
+            - Gets the time when the assembly has been completed.
+            - Displays a message to the operator that the assembly is completed
+            - Sets the state of the GUI to the display_wo_requirements state
+        """
         self.ended_at = datetime.datetime.now()
         self.summary_status = True
         self.upload_report()
@@ -220,11 +222,17 @@ class Ui_Dialog(object):
         msg.setWindowTitle("Information MessageBox")
         msg.setStandardButtons(QMessageBox.Ok)
         retval = msg.exec_()
-        self.counter = 0
-        self.method_list = [self.verify_user, self.verify_pn_sn, self.verify_assembly_part]
-        self.display_wo()
+        self.method_list_counter = 0
+        self.test_method_list = [self.verify_user, self.verify_pn_sn, self.verify_assembly_part]
+        self.display_wo_requirements()
 
     def fail_test(self):
+        """Helper method used whehn operator wants to fail a test
+
+            - Gets the time when the assembly has been completed.
+            - Displays a message to the operator that the assembly has failed
+            - Sets the state of the GUI to the display_wo_requirements state
+        """
         self.ended_at = datetime.datetime.now()
         self.summary_status = False
         self.upload_report()
@@ -234,14 +242,29 @@ class Ui_Dialog(object):
         msg.setWindowTitle("Information MessageBox")
         msg.setStandardButtons(QMessageBox.Ok)
         retval = msg.exec_()
-        self.counter = 0
-        self.method_list = [self.verify_user, self.verify_pn_sn, self.verify_assembly_part]
-        self.display_wo()
+        self.method_list_counter = 0
+        self.test_method_list = [self.verify_user, self.verify_pn_sn, self.verify_assembly_part]
+        self.display_wo_requirements()
+
+    def verify_user_input(self):
+        """Helper method used to display message box when user input fails to be verified
+        """
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Information)
+        msg.setText("Incorrect Input")
+        msg.setWindowTitle("Information MessageBox")
+        msg.setStandardButtons(QMessageBox.Ok)
+        retval = msg.exec_()
 
     def verify_user(self):
-        Operator.user_name = self.lineEdit.text()
+        """Verifies that the username entered exits in the Operator table
+
+        If user exists then the text in the Verified column in the username row will equal "yes" and column background
+         color will display green
+        """
+        self.operator_user_name = self.lineEdit.text()
         self.start()
-        query = f"SELECT EXISTS(SELECT * from operator WHERE user_name='{Operator.user_name}');"
+        query = f"SELECT EXISTS(SELECT * from operator WHERE user_name='{self.operator_user_name}');"
         self.cursor.execute(query)
         ret = self.cursor.fetchall()[0][0]
         self.end()
@@ -249,18 +272,24 @@ class Ui_Dialog(object):
             self.tableWidget.setItem(0, 1, QtWidgets.QTableWidgetItem("Yes"))
             self.tableWidget.item(0, 1).setBackground(QtGui.QColor(0, 255, 0))
             self.lineEdit.clear()
-            self.operator_label.setText(f"User Name: {Operator.user_name}")
-            # self.factory_id_label.setText(f"Factory ID: {}")
-            self.get_user_info()
+            self.operator_label.setText(f"User Name: {self.operator_user_name}")
+            self.get_user_permission_status()
+        else:
+            self.verify_user_input()
+            self.lineEdit.clear()
+            self.method_list_counter -= 1
 
-    def get_user_info(self):
+    def get_user_permission_status(self):
+        """Queries Operator table for user's permission status and assigns results in instance variable(permission)
+        """
         self.start()
-        query = f"SELECT position from operator WHERE user_name='{Operator.user_name}';"
+        query = f"SELECT position from operator WHERE user_name='{self.operator_user_name}';"
         self.cursor.execute(query)
         self.permission = self.cursor.fetchall()[0][0]
         self.end()
 
     def get_rout_info(self):
+        """Queries Routs table to populate the factory id and rout name labels"""
         self.start()
         query = f"SELECT rout_name, factory_id from routs WHERE station_name = '{self.station_name}';"
         self.cursor.execute(query)
@@ -271,6 +300,13 @@ class Ui_Dialog(object):
         self.rout_name_label.setText(f"Rout Name: {self.rout_name}")
 
     def verify_pn_sn(self):
+        """Verifies the DUT's part number and serial number
+
+            - Checks if part number and serial number exist in the WorkOrder table
+            - If exists then display yes in verified column with background color set to green
+            - Query product name from WorkOrder table and assign to instance variable
+            - If username equals "admin" then enable combobox and set superuser checkbox True
+        """
         self.start()
         self.dut_pn, self.dut_sn = self.lineEdit.text().split(",")
         query = f"SELECT EXISTS(SELECT * from WorkOrder WHERE part_number='{self.dut_pn}' and serial_number='{self.dut_sn}');"
@@ -286,6 +322,10 @@ class Ui_Dialog(object):
             self.cursor.execute(query)
             self.product_name = self.cursor.fetchall()[0][0]
             self.end()
+        else:
+            self.verify_user_input()
+            self.lineEdit.clear()
+            self.method_list_counter -= 1
         if self.permission == "admin":
             self.su_checkBox.setChecked(True)
             self.test_comboBox.show()
@@ -299,6 +339,9 @@ class Ui_Dialog(object):
             self.load_assembly()
 
     def get_rout_size(self):
+        """Queries assembly_list for the rout size then assigns to instance variable and resets the GUI back to
+        display_wo_requirements(beginning) when rout is completed.
+        """
         self.start()
         query = (f"SELECT complete_rout_size FROM assembly_list WHERE product = '{self.product_name}';")
         self.cursor.execute(query)
@@ -313,13 +356,16 @@ class Ui_Dialog(object):
             msg.setStandardButtons(QMessageBox.Ok)
             retval = msg.exec_()
             self.assembly_count = 0
-            self.counter = -1
-            self.display_wo()
+            self.method_list_counter = -1
+            self.display_wo_requirements()
             self.rout_done = True
         else:
             self.rout_done = False
 
     def verify_assembly_part(self):
+        """Check the user input against the regex assigned to the assembly part and sets the verified column to yes
+        if verified.
+        """
         self.started_at = datetime.datetime.now()
         pn, sn = self.lineEdit.text().split(",")
         if bool(re.fullmatch(self.assembly_list[self.assembly_count].pn_rx, pn)) and \
@@ -340,7 +386,8 @@ class Ui_Dialog(object):
 
     def check_rout(self):
         self.start()
-        query = (f"SELECT max(rout_position), status FROM StationReport WHERE dut_serial_number = '{self.dut_sn}' group by status;")
+        query = (
+            f"SELECT max(rout_position), status FROM StationReport WHERE dut_serial_number = '{self.dut_sn}' group by status;")
         self.cursor.execute(query)
         position = self.cursor.fetchall()
         print(position)
@@ -354,6 +401,8 @@ class Ui_Dialog(object):
                 self.rout_position = int(position[0][0])
 
     def load_assembly(self):
+        """Queries the assembly_list table for all assembly parts that are required to complete the station
+         and loads the information in the assembly table"""
         # TODO add try except
         self.start()
         query = (
@@ -373,6 +422,11 @@ class Ui_Dialog(object):
         self.get_rout_info()
 
     def get_first_pass(self):
+        """Checks if the product has been at the station
+
+            - Queries StationReport table for existing station record of product
+            - If product record exist for the selected station then first pass equals False else True
+        """
         self.start()
         first_pass = f"select exists(select first_pass from StationReport where station_name = '{self.station_name}');"
         self.cursor.execute(first_pass)
@@ -385,9 +439,11 @@ class Ui_Dialog(object):
         self.end()
 
     def upload_report(self):
+        """Uploads a test record result to the StationReport table.
+        """
         self.start()
         query = f"INSERT INTO StationReport (station_name, operator_user_name, dut_part_number, dut_serial_number, status, started_at, ended_at, rout_position, complete_rout_size, rout_name, first_pass) VALUES" \
-                f" ('{self.station_name}', '{Operator.user_name}','{self.dut_pn}', '{self.dut_sn}', {self.summary_status}, '{self.started_at}', '{self.ended_at}', '{self.rout_position}' ,'{self.rout_position}', '{self.rout_name}', {self.first_pass});"
+                f" ('{self.station_name}', '{self.operator_user_name}','{self.dut_pn}', '{self.dut_sn}', {self.summary_status}, '{self.started_at}', '{self.ended_at}', '{self.rout_position}' ,'{self.rout_position}', '{self.rout_name}', {self.first_pass});"
         self.cursor.execute(query)
         self.cnx.commit()
 
